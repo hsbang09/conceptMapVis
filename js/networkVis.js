316933341455da1202b842f41da0e898b7e941a2
Bang <hb398@cornell.edu>
2019-05-19 01:13:21 -0400

    Adds the capability to add new edges interactively

--- js/networkVis.js
+++ js/networkVis.js
@@ -1,8 +1,9 @@
 
-
+var data = null;
 var network = null;
 var nodes = null;
-let edges = null;
+var edges = null;
+var newEdges = new vis.DataSet([]);
 
 let instruments = ['OCE_SPEC', 'AERO_POL','SAR_ALTIM', 'VEG_INSAR', 'CPR_RAD', 'HYP_IMAG', 'OCE_SPEC',
                                 'HIRES_SOUND', 'VEG_LID', 'CHEM_SWIRSPEC', 'HYP_ERB', 'AERO_LID', 'CHEM_UVSPEC'];
@@ -24,19 +25,13 @@ var contexMenu = null;
 
 $.getJSON("visData/ClimateCentric-4.json", (d) => {
 
-    var data = d;
+    data = d;
 
     // create an array with nodes
     nodes = new vis.DataSet(data.nodes);
 
     // create an array with edges
     edges = new vis.DataSet(data.edges);
-
-    // subscribe to any change in the DataSet
-    edges.on('*', function (event, properties, senderId) {
-      console.log('event', event, properties);
-    });
-
 
     for(let i = 0; i < data.nodes.length; i++){
         let node = data.nodes[i];
@@ -52,32 +47,22 @@ $.getJSON("visData/ClimateCentric-4.json", (d) => {
         node.group = groupIndex;
     }
 
-
     // create a network
     var container = document.getElementById('networkContainer');
 
     var options = {
             manipulation: {
                 enabled: false,
-                addNode: function (data, callback) {
-                    // filling in the popup DOM elements
-                    console.log('add', data);
-                },
-                editNode: function (data, callback) {
-                    // filling in the popup DOM elements
-                    console.log('edit', data);
-                },
+                // addNode: function (data, callback) {
+                //     // filling in the popup DOM elements
+                //     console.log('add', data);
+                // },
+                // editNode: function (data, callback) {
+                //     // filling in the popup DOM elements
+                //     console.log('edit', data);
+                // },
                 addEdge: function (data, callback) {
-                    console.log('add edge', data);
-                    if (data.from == data.to) {
-                        var r = confirm("Do you want to connect the node to itself?");
-                        if (r === true) {
-                            callback(data);
-                        }
-                    } 
-                    else {
-                        callback(data);
-                    }
+                    addNewEdge(data, callback);
                 }
             }
         };
@@ -97,14 +82,8 @@ $.getJSON("visData/ClimateCentric-4.json", (d) => {
             console.log("edge");
             console.log(edgeID);
             this.selectEdges([edgeID]);
-        }
-
-
-        
+        }        
     });
-
-
-
 });
 
 
@@ -112,7 +91,6 @@ function addConnections(elem, index) {
     // need to replace this with a tree of the network, then get child direct children of the element
     elem.connections = network.getConnectedNodes(index);
 }
-
 
 
 var myNodes = null;
@@ -128,29 +106,87 @@ function parseNetwork(){
     myNodes= nodes;
 }
 
-
-// // create an array with nodes
-// var nodes = new vis.DataSet([
-//   {id: '1a', label: 'Node 1'},
-//   {id: '2b', label: 'Node 2'},
-//   {id: '3c', label: 'Node 3'},
-//   {id: '4d', label: 'Node 4'},
-//   {id: '5e', label: 'Node 5'}
-// ]);
-
-// // create an array with edges
-// var edges = new vis.DataSet([
-//   {from: '1a', to: '3c', label:'abc'},
-//   {from: '1a', to: '2b', label:'bcd'},
-//   {from: '2b', to: '4d'},
-//   {from: '2b', to: '5e'},
-//   {from: '3c', to: '3c'}
-// ]);
-
 function objectToArray(obj) {
     return Object.keys(obj).map(function (key) {
       obj[key].id = key;
       return obj[key];
     });
+}
+
+function addNewEdge(data, callback){
+
+    var title = "Adding a new link between " + getNodeLabel(data.from) + " and " + getNodeLabel(data.to) + "\n";
+
+    var linkTypeInput = '<select>'
+                            + '<option value="select"> Select </option>'
+                            + '<option value="hasSynergyWith"> hasSynergyWith </option>'
+                            + '<option value="mustAvoid"> mustAvoid </option>'
+                        + '</select>'
+
+    let buttonsStyle = "width: 150px;" +
+                    "float: left;";
+
+    var inputCallback = function(){
+        var inputs = d3.selectAll(".iziToast-inputs-child.revealIn").nodes();
+        var connectionType = inputs[0].value;
+        var weight = inputs[1].value;
+
+        var confirmButton = d3.select("#iziToast_button_confirm");
+        if(connectionType !== "select" && weight !== ""){
+            // Activate confirm button                    
+            confirmButton.node().disabled = false;
+            confirmButton.select('b').style("opacity", "1.0");
+        } else {
+            var confirmButton = d3.select("#iziToast_button_confirm");
+            confirmButton.node().disabled = true;
+            confirmButton.select('b').style("opacity", "0.05");
+        }
+    }
+
+    iziToast.question({
+        drag: false,
+        timeout: false,
+        close: false,
+        overlay: true,
+        displayMode: 0,
+        id: 'question',
+        progressBar: false,
+        title: title,
+        message: 'Please select the connection type and the corresponding weight',
+        position: 'center',
+        inputs: [
+            [linkTypeInput, 'change', function (instance, toast, select, event) {
+                inputCallback();
+            }],
+            ['<input type="number">', 'keyup', function (instance, toast, input, event) {
+                inputCallback();
+            }],
+        ],
+        buttons: [
+            ['<button id="iziToast_button_confirm" disabled><b style="opacity: 0.05">Confirm</b></button>', function (instance, toast, button, event, inputs) {
+                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                var connectionType = inputs[0].options[inputs[0].selectedIndex].value;
+                var weight = inputs[1].value;
+                data.label = connectionType;
+                data.weight = weight;
+                newEdges.add(data);
+                callback(data);
+
+            }, false], // true to focus
+            ['<button id="iziToast_button_cancel">Cancel</button>', function (instance, toast, button, e) {
+                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+            }]
+        ],
+    });
+}
+
+function getNodeLabel(nodeID){
+    var nodeData = data.nodes;
+    for(var i = 0; i < nodeData.length; i++){
+        if(nodeData[i].id === nodeID){
+            return nodeData[i].label;
+        }
+    }
+    return null;
 }
 
